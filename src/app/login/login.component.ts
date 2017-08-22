fa8c84a33faa13ee462eeb44bb4179bd371e30e0
Alex Li <itintern1@sdcdom.local>
2017-08-22 10:37:59 +1000

    more comment

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,21 +14,28 @@ const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA
 
 
 export class LoginComponent implements OnInit {
+  
+  // for basic setting
   public errors;
-  public logoPath:string;    
+  public logoPath:string;
   color: string = 'primary';
   determinateProgressValue: number = 30;
 
+  // initial part
   constructor(private router: Router) {
     this.logoPath = '../../assets/img/logo.jpg';
     this.errors = "true";
   }
-  ngOnInit() {
-  }
+
+  ngOnInit() {}
+
+
   emailFormControl = new FormControl('', [Validators.required, Validators.pattern(EMAIL_REGEX)]);
+  
   stepDeterminateProgressVal(val: number) {
     this.determinateProgressValue = this.clampValue(val + this.determinateProgressValue);
   }
+  
   private clampValue(value: number) {
     return Math.max(0, Math.min(100, value));
   }
