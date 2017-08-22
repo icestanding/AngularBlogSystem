c6d8a2668d218ffa96aa6029fc74f6a12fc7411f
Alex Li <itintern1@sdcdom.local>
2017-08-22 11:25:55 +1000

    fix div adjust to window

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, Routes } from '@angular/router';
 import {FormControl, Validators} from '@angular/forms';
-
+// import { NgIf } from '@angular/common';
 
 const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
@@ -18,13 +18,16 @@ export class LoginComponent implements OnInit {
   // for basic setting
   public errors;
   public logoPath:string;
+  public hidden:boolean;
   color: string = 'primary';
   determinateProgressValue: number = 30;
+ 
 
   // initial part
   constructor(private router: Router) {
     this.logoPath = '../../assets/img/logo.jpg';
     this.errors = "true";
+    this.hidden = false;
   }
 
   ngOnInit() {}
@@ -35,8 +38,11 @@ export class LoginComponent implements OnInit {
   stepDeterminateProgressVal(val: number) {
     this.determinateProgressValue = this.clampValue(val + this.determinateProgressValue);
   }
-  
+
   private clampValue(value: number) {
     return Math.max(0, Math.min(100, value));
+  }
+  signUp () {
+    this.hidden = true;
   }
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 import { LoginComponent } from './login.component';
 import { HomeModule } from './login-routing.module'
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MdToolbarModule } from '@angular/material';
 import { MdInputModule } from '@angular/material';
-// import { MdButton } from '@angular/material';
 // for form control
-import {FormsModule,  ReactiveFormsModule} from '@angular/forms';
+import { FormsModule,  ReactiveFormsModule } from '@angular/forms';
 import { MdButtonModule } from '@angular/material';
 import { MdCardModule } from '@angular/material';
 import { MdProgressBarModule } from '@angular/material';
@@ -16,15 +16,16 @@ import { MdProgressBarModule } from '@angular/material';
 @NgModule({
   imports: [
     CommonModule,
+    BrowserModule,
     HomeModule,
     BrowserAnimationsModule,
     MdToolbarModule,
     MdInputModule,
     FormsModule,
     MdButtonModule,
-    // MdButton,
     ReactiveFormsModule,
-    MdCardModule
+    MdCardModule,
+    MdProgressBarModule
   ],
   declarations: [LoginComponent],
   exports: [LoginComponent]
