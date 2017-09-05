742c3d22b37b77ebd00b8ca7a7e09f4bd150ee1f
Alex Li <itintern1@sdcdom.local>
2017-09-05 10:55:11 +1000

    simple scroll"

--- src/app/share/header/nav/nav.component.ts
+++ src/app/share/header/nav/nav.component.ts
@@ -1,18 +1,38 @@
-import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit, HostListener,Inject } from '@angular/core';
+import { DOCUMENT }from '@angular/platform-browser'
 
 @Component({
-  selector: 'navigation',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+    selector: 'navigation',
+    templateUrl: './nav.component.html',
+    styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
-  @Output () event = new EventEmitter();
-  constructor() { }
 
-  ngOnInit() {
-  }
-  toggle(e) {
-    this.event.emit(e);
-  }
+    @Output () event = new EventEmitter();
+    public transform:{
+      'nav'?: boolean;
+      'navfixed'?: boolean;
+    };
+    constructor(@Inject(DOCUMENT) private document:Document) {
+      this.transform = {'nav':true, 'navfixed': false};
+    }
+    ngOnInit() {}
+    
+    @HostListener('window:scroll', [])
+    onWindowScroll() {
+      let number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+      if(number > 200) {
+        this.transform['nav'] = false;
+        this.transform['navfixed'] = true;
+      }
+      else if (number <= 200) {
+        this.transform['nav'] = true;
+        this.transform['navfixed'] = false;
+      }
+    }
 
+    toggle(e) {
+      this.event.emit(e);
+      
+    }
 }
