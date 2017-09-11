4157bffbd02b971aa8492e6d4061522cc3bd084f
Chenyu <lichenyu119@gmail.com>
2017-09-11 10:57:06 +1000

    [backend]fix babel problem

--- server/server.js
+++ server/server.js
@@ -1,15 +1,35 @@
 import Koa from 'koa';
+
+// to support stupid function
+require("babel-core/register");
+require("babel-polyfill");
+
 import Router from 'koa-router';
-import ServerStatic from "koa-static";
+import ServerStatic from 'koa2-static';
+import send from 'koa-send';
+import path from 'path';
+// let regeneratorRuntime =  require("regenerator-runtime");
 
 
+
+
+
+console.log("cnm");
 
 let server = new Koa();
 let main = new Router();
 
-// resend file, then request
-//
-// main.use(async(ctx) =>
-//
-// )
 
+server.use(ServerStatic({
+  path: "/",
+  root: __dirname + "/dist"
+}));
+
+
+
+console.log(__dirname);
+
+// main.get('/', async (ctx)=> {
+//
+//   // await send(ctx, ctx.path);
+// })
