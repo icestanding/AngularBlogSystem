5979961425ba527a336822da580c809accc8ebcc
Chenyu <lichenyu119@gmail.com>
2017-09-13 09:32:22 +1000

    [frontend]include quill editor

--- src/app/admin/admin/admin.module.ts
+++ src/app/admin/admin/admin.module.ts
@@ -4,7 +4,8 @@ import { AdminComponent } from "./admin.component";
 import {BrowserModule} from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AdminRoutingModule } from "./admin-routing.module";
-import { MdButtonModule } from '@angular/material'
+import { MdButtonModule } from '@angular/material';
+import { QuillModule } from 'ngx-quill'
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { MdButtonModule } from '@angular/material'
    AdminRoutingModule,
    BrowserModule,
    HttpClientModule,
-   MdButtonModule
+   MdButtonModule,
+   QuillModule
   ],
   declarations: [
     AdminComponent
