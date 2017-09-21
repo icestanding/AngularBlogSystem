5f6718ef4e8cbe3733f1cd515e4c4921cd848e7c
Chenyu <lichenyu119@gmail.com>
2017-09-21 11:37:38 +1000

    [frontend]finish draft design of blog quick look

--- src/app/front/mainpage/mainpage.module.ts
+++ src/app/front/mainpage/mainpage.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainpageComponent } from './mainpage.component';
 // import { MainpageRoutingModule } from  './mainpage-routing.module';
-import { MdCardModule,MdListModule } from '@angular/material'
+import { MdCardModule,MdListModule,MdButtonModule } from '@angular/material'
 
 @NgModule({
   imports: [
     CommonModule,
     // MainpageRoutingModule,
     MdCardModule,
-    MdListModule
+    MdListModule,
+    MdButtonModule
   ],
   declarations: [MainpageComponent]
 })
