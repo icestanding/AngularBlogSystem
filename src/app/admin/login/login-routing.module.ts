9b8e22b5297afbd8f98accd7de9b5b9622027ca2
Chenyu <lichenyu119@gmail.com>
2017-09-21 17:04:55 +1000

    [frontend]don't know how to implement redirect with guard

--- src/app/admin/login/login-routing.module.ts
+++ src/app/admin/login/login-routing.module.ts
@@ -2,11 +2,16 @@
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './login.component';
 import { RouterModule } from '@angular/router'
+import { LoginRouterGuard } from '../../guard/loginguard'
 
 
 
 @NgModule({
-  imports: [RouterModule.forChild([{path: 'login', component: LoginComponent}]),
+  imports: [RouterModule.forChild([
+    
+    // {path: 'login', component: LoginComponent, redirectTo:'admin', canActivate:[LoginRouterGuard]},
+    {path: 'login', component: LoginComponent}
+  ]),
   ],
   exports:[RouterModule]
 })
