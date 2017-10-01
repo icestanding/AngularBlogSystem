b87d43810d7fc360ce439587786b8705b2a83af6
Chenyu <lichenyu119@gmail.com>
2017-10-02 00:38:49 +1100

    [other]give up animation before next version

--- src/app/front/router.animation.ts
+++ src/app/front/router.animation.ts
@@ -1,16 +1,16 @@
 import {trigger, animate, style, group, query, transition, state} from '@angular/animations';
 export const routerTransition = trigger('routerTransition', [
   transition('* <=> *', [
-    // query(':enter, :leave', style({ position: 'fixed', width:'100%',color:"red" })
-    //   , { optional: true }),
+    query(':enter, :leave', style({position: 'relative', opacity: 0})
+      , { optional: true }),
     group([
       query(':enter', [
-        style({ transform: 'translateX(100%)' }),
-        animate('0.5s ease-in-out', style({ transform: 'translateX(0%)' }))
+        style({ transform: 'translateX(20%)', 'background-color': 'yellow'  }),
+        animate('3s ease-in-out', style({ transform: 'translateX(0%)' }))
       ]),
       query(':leave', [
-        style({ transform: 'translateX(0%)' }),
-        animate('0.5s ease-in-out', style({ transform: 'translateX(-100%)' }))
+        style({ transform: 'translateX(0%)','background-color': 'yellow'  }),
+        animate('3s ease-in-out', style({ transform: 'translateX(-100%)','background-color': 'yellow'  }))
       ]),
     ])
   ])
